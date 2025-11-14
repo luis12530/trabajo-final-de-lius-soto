34ebd899129b128f1126ed49882bb3240c641154
luis12530 <sotoluiseduardo2002@gmail.com>
2025-11-14 19:17:39 -0300

    primer commit

---
+++ src/main/java/ar/edu/ies6/trabajofinalsotoies6/Trabajofinalsotoies6Application.java
@@ -0,0 +1,13 @@
+package ar.edu.ies6.trabajofinalsotoies6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trabajofinalsotoies6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Trabajofinalsotoies6Application.class, args);
+	}
+
+}
